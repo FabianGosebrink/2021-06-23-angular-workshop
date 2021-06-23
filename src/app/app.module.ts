d0d94bfdc7115fe5ed46c55a9d627a72db65d6be
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2021-06-23 09:28:56 +0200

    added another component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
 import { SharedModule } from './shared/shared.module';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [AppComponent, ContentComponent],
+  declarations: [AppComponent, ContentComponent, TodoListComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
+})
+export class TodoListComponent implements OnInit {
+  @Input() items: string[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
